fd08c4e4cb052ded8446be1396dea7f8e010bbdc
Vadym Baranov <vadym.baranov@hotmail.com>
2024-12-30 23:04:14 +0200

    it moves (player moves)

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    x = SCREEN_WIDTH / 1
-    y = SCREEN_HEIGHT / 1
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
